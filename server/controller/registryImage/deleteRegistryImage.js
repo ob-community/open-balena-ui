2ae25f2379b52ed6fc57b7f0157e73a5068ba7bc
Shaun Cooley <scooley@mapped.com>
2025-04-04 10:33:53 -0700

    fix: s3Client and bucketNames.registry now instead of minioClient/registryBucket

--- server/controller/registryImage/deleteRegistryImage.js
+++ server/controller/registryImage/deleteRegistryImage.js
@@ -1,31 +1,29 @@
 const { s3Client, bucketNames } = require('../../util/s3');
 
-module.exports = (imageLocationHash) => new Promise((resolve, reject) => {
+module.exports = (imageLocationHash) =>
+  new Promise((resolve, reject) => {
+    const imageRepository = `data/docker/registry/v2/repositories/v2/${imageLocationHash}`;
+    const objectsList = [];
+    const objectsStream = s3Client.listObjects(bucketNames.registry, imageRepository, true);
 
-  const imageRepository = `data/docker/registry/v2/repositories/v2/${imageLocationHash}`;
-  const objectsList = [];
-  const objectsStream = s3Client.listObjects(bucketNames.registry, imageRepository, true);
+    objectsStream.on('data', (obj) => {
+      objectsList.push(obj.name);
+    });
 
-  objectsStream.on('data', (obj) => {
-    objectsList.push(obj.name);
-  });
+    objectsStream.on('error', (err) => {
+      reject(err);
+    });
 
-  objectsStream.on('error', (err) => {
-    reject(err);
-  });
-
-  objectsStream.on('end', () => {
-    if (objectsList.length === 0) {
-      reject(new Error ('image not found'));
-    }
-    minioClient.removeObjects(registryBucket, objectsList, (err) => {
-      if (err) {
-        reject(err);
+    objectsStream.on('end', () => {
+      if (objectsList.length === 0) {
+        reject(new Error('image not found'));
       }
-      else {
-        resolve();
-      }
+      s3Client.removeObjects(bucketNames.registry, objectsList, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   });
-
-});
